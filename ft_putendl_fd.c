866863b85403890339fb31354428e01857836a52
danielfdez17 <daniel17dev@gmail.com>
2025-10-09 17:05:05 +0200

    feat: added unistd lib to libft

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -10,24 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include "libft.h"
-
-// static void	ft_putchar_fd(char c, int fd)
-// {
-// 	write(fd, &c, 1);
-// }
-
-// static void	ft_putstr_fd(char const *s, int fd)
-// {
-// 	if (!s)
-// 		return ;
-// 	while (*s)
-// 	{
-// 		ft_putchar_fd(*s, fd);
-// 		s++;
-// 	}
-// }
 
 void	ft_putendl_fd(char const *s, int fd)
 {
