66752665e891bfd78b4851124e66181f9b3f7927
danielfdez17 <daniel17dev@gmail.com>
2025-10-09 17:06:48 +0200

    feat: removed unnecesary code

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -10,13 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include "libft.h"
-
-// static void	ft_putchar_fd(char c, int fd)
-// {
-// 	write(fd, &c, 1);
-// }
 
 void	ft_putstr_fd(char const *s, int fd)
 {
