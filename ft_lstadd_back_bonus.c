fb65d6dc9f9cb584c498b7de21cbf18c5b5c0ae2
danielfdez17 <daniel17dev@gmail.com>
2025-10-09 16:42:37 +0200

    fix: removed useless comment

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -12,21 +12,6 @@
 
 #include "libft.h"
 
-// t_list	*ft_lstlast(t_list *lst)
-// {
-// 	t_list	*ptr;
-// 	t_list	*prev;
-
-// 	ptr = lst;
-// 	prev = NULL;
-// 	while (ptr != NULL)
-// 	{
-// 		prev = ptr;
-// 		ptr = ptr->next;
-// 	}
-// 	return (prev);
-// }
-
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*ptr;
