55cd122f09c00a0ccc65a8922e921c5517c49507
danielfdez17 <daniel17dev@gmail.com>
2025-10-09 17:23:56 +0200

    refactor: ft_split

--- ft_split.c
+++ ft_split.c
@@ -36,69 +36,114 @@ static int	ft_count_words(char const *s, char c)
 	return (words);
 }
 
-static char	*ft_get_segment(char const *s, int i, int letters)
-{
-	char	*result;
-	int		j;
+// static char	*ft_get_segment(char const *s, int i, int letters)
+// {
+// 	char	*result;
+// 	int		j;
 
-	j = 0;
-	result = malloc(sizeof(char) * (letters + 1));
-	if (!result)
-		return (NULL);
-	while (j < letters)
-	{
-		result[j] = s[i - letters + j];
-		++j;
-	}
-	result[j] = '\0';
-	return (result);
+// 	j = 0;
+// 	result = malloc(sizeof(char) * (letters + 1));
+// 	if (!result)
+// 		return (NULL);
+// 	while (j < letters)
+// 	{
+// 		result[j] = s[i - letters + j];
+// 		++j;
+// 	}
+// 	result[j] = '\0';
+// 	return (result);
+// }
+
+// /**
+//  * @var counters[0] to iterate the whole string @param s
+//  * @var counters[1] to iterate every word of @param s
+//  * @var counters[2] to know the letters of every word of @param s
+//  */
+// static void	ft_aux_get_seg(char const *s, char **result, int *counters)
+// {
+// 	if (counters[2] != 0)
+// 	{
+// 		result[counters[1]] = ft_get_segment(s, counters[0], counters[2]);
+// 		++counters[1];
+// 	}
+// }
+
+// /**
+//  * @var counters[0] to iterate the whole string @param s
+//  * @var counters[1] to iterate every word of @param s
+//  * @var counters[2] to know the letters of every word of @param s
+//  */
+// char	**ft_split(char const *s, char c)
+// {
+// 	char	**result;
+// 	int		counters[3];
+
+// 	counters[0] = 0;
+// 	counters[1] = 0;
+// 	counters[2] = 0;
+// 	result = malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
+// 	if (!result)
+// 		return (NULL);
+// 	while (s[counters[0]])
+// 	{
+// 		if ((unsigned char) s[counters[0]] != (unsigned char) c)
+// 			++counters[2];
+// 		else
+// 		{
+// 			ft_aux_get_seg(s, result, counters);
+// 			counters[2] = 0;
+// 		}
+// 		++counters[0];
+// 	}
+// 	ft_aux_get_seg(s, result, counters);
+// 	result[counters[1]] = NULL;
+// 	return (result);
+// }
+
+char	**free_result(char **result, size_t w_counter)
+{
+	while (--w_counter)
+		free(result[w_counter]);
+	free(result);
+	return (NULL);
 }
 
-/**
- * @var counters[0] to iterate the whole string @param s
- * @var counters[1] to iterate every word of @param s
- * @var counters[2] to know the letters of every word of @param s
- */
-static void	ft_aux_get_seg(char const *s, char **result, int *counters)
+void	ft_init_counters(unsigned int* i, size_t* w_c, size_t* l_c)
 {
-	if (counters[2] != 0)
-	{
-		result[counters[1]] = ft_get_segment(s, counters[0], counters[2]);
-		++counters[1];
-	}
+	*i = 0;
+	*w_c = 0;
+	*l_c = 0;
 }
 
-/**
- * @var counters[0] to iterate the whole string @param s
- * @var counters[1] to iterate every word of @param s
- * @var counters[2] to know the letters of every word of @param s
- */
 char	**ft_split(char const *s, char c)
 {
-	char	**result;
-	int		counters[3];
+	char			**result;
+	unsigned int 	i;
+	size_t			w_counter;
+	size_t			l_counter;
 
-	counters[0] = 0;
-	counters[1] = 0;
-	counters[2] = 0;
+	ft_init_counters(&i, &w_counter, &l_counter);
 	result = malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
 	if (!result)
 		return (NULL);
-	while (s[counters[0]])
+	while (s[i])
 	{
-		if ((unsigned char) s[counters[0]] != (unsigned char) c)
-			++counters[2];
+		if ((unsigned char) s[i] != (unsigned char) c)
+			++l_counter;
 		else
 		{
-			ft_aux_get_seg(s, result, counters);
-			counters[2] = 0;
+			result[w_counter] = ft_substr(s, i, l_counter);
+			if (!result[w_counter])
+				return (free_result(result, w_counter));
+			++w_counter;
+			l_counter = 0;
 		}
-		++counters[0];
+		++i;
 	}
-	ft_aux_get_seg(s, result, counters);
-	result[counters[1]] = NULL;
+	result[w_counter] = NULL;
 	return (result);
 }
+
 
 // #include <stdio.h>
 // int main(void)
