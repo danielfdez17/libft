57e5273eaa10205dd9204c8e6685897106871175
danfern3 <danfern3@student.42madrid.com>
2025-10-10 08:57:30 +0200

    fix: restored ft_lstmap to first correction

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: danfern3 <danfern3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 07:46:24 by danfern3          #+#    #+#             */
-/*   Updated: 2025/10/03 12:53:35 by danfern3         ###   ########.fr       */
+/*   Updated: 2025/10/10 08:56:27 by danfern3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,16 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	new_list = NULL;
 	while (lst)
 	{
-		new_elem = (t_list *)f(lst->content);
-		new_elem = ft_lstnew(new_elem);
+		new_elem = ft_lstnew((t_list *)f(lst->content));
 		if (!new_elem)
 		{
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_list, new_elem);
+		if (new_list)
+			ft_lstadd_back(&new_list, new_elem);
+		else
+			new_list = new_elem;
 		lst = lst->next;
 	}
 	return (new_list);
