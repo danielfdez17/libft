f4dcd66e775be3376870e0295c41bb44cabaec9a
danfern3 <danfern3@student.42madrid.com>
2025-10-10 08:40:27 +0200

    fix: strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: danfern3 <danfern3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 07:45:53 by danfern3          #+#    #+#             */
-/*   Updated: 2025/10/10 07:34:35 by danfern3         ###   ########.fr       */
+/*   Updated: 2025/10/10 08:39:20 by danfern3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ static int	ft_is_in_set(char c, char const *set)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*result;
 	size_t	start;
 	size_t	end;
 
@@ -37,17 +36,15 @@ char	*ft_strtrim(char const *s1, char const *set)
 		++start;
 	while (ft_is_in_set(s1[end], set) == 1 && end >= start)
 		--end;
-	result = malloc(sizeof(char) * (end - start + 1));
-	if (!result)
-		return (NULL);
 	return (ft_substr(s1, start, end - start + 1));
 }
 
 // #include <stdio.h>
 // int main(void)
 // {
+// 	char *result;
 // 	printf("%s|\n---\n", \
-// ft_strtrim("lorem \n ipsum \t dolor \n sit \t amet", " "));
+// (	result = ft_strtrim("lorem \n ipsum \t dolor \n sit \t amet", " ")));
 // 	printf("%s|\n---\n", ft_strtrim("lorem ipsum dolor sit amet", "te"));
 // 	printf("%s|\n---\n", ft_strtrim(" lorem ipsum dolor sit amet", "l "));
 // 	printf("%s|\n---\n", ft_strtrim("lorem ipsum dolor sit amet", "tel"));
